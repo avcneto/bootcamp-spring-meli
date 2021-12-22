683b84f611a91bfe651a90c56b90aa2add79b421
Aderson <aderson.neto@mercadolivre.com>
2021-12-22 13:20:42 -0300

    adding romanos number

--- src/main/java/com/example/bootcamp/meli/bootcampjavaspringmeli/controller/Test.java
+++ src/main/java/com/example/bootcamp/meli/bootcampjavaspringmeli/controller/Test.java
@@ -1,14 +1,21 @@
 package com.example.bootcamp.meli.bootcampjavaspringmeli.controller;
 
+import com.example.bootcamp.meli.bootcampjavaspringmeli.model.NumeroRomanos;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
 
-    @GetMapping("/test")
-    public String test() {
-        return "teste";
-    }
+    @GetMapping("/converterromanos/{number}")
+    public ResponseEntity<String> getNumeroRomano(@PathVariable Integer number) {
+        String result = NumeroRomanos.intToRoman(number);
 
+        if (number != null) {
+            return ResponseEntity.ok(result);
+        }
+        return ResponseEntity.status(404).build();
+    }
 }
