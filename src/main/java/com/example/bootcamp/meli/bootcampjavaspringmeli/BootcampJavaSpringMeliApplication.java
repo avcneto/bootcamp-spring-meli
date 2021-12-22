a64841c8e7b7ecf5fd51bfabec6a6d184ffe5808
Aderson <deco.neto10@gmail.com>
2021-12-22 11:42:28 -0300

    first commit

---
+++ src/main/java/com/example/bootcamp/meli/bootcampjavaspringmeli/BootcampJavaSpringMeliApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp.meli.bootcampjavaspringmeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJavaSpringMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampJavaSpringMeliApplication.class, args);
+	}
+
+}
